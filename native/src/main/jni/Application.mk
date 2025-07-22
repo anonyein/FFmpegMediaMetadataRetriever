5503f49900dc3ef2f6bed3f3e5c7e68e436b9b26
Vigli Lee <vigli@kmong.com>
2025-07-22 16:48:10 +0900

    ff

--- native/src/main/jni/Application.mk
+++ native/src/main/jni/Application.mk
@@ -3,3 +3,9 @@
 APP_ABI := armeabi-v7a x86 x86_64 arm64-v8a
 APP_STL := c++_shared
 APP_CPPFLAGS := -std=c++11
+APP_SUPPORT_FLEXIBLE_PAGE_SIZES := true
+
+# 16KB page size alignment support
+APP_LDFLAGS += -Wl,-z,max-page-size=16384
+APP_LDFLAGS += -Wl,-z,common-page-size=16384
+APP_LDFLAGS += -Wl,--hash-style=sysv
